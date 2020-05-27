b9d141e581053812f1e883f4d5478537326e3541
duanbaihhu <duanbaihu>
2020-05-27 18:17:49 +0800

    add

--- module/user/ext/model/identify.php
+++ module/user/ext/model/identify.php
@@ -3,15 +3,19 @@ public function identify($account, $password)
 {
 	$user=false; 
     $ldap = $this->loadModel('ldap');
+    echo $this->config->ldap->ldapOpen;
 	if($this->config->ldap->ldapOpen){ 
 		if($account!="admin"){
 		    $user = $ldap->userauth($account, $password);
+		    echo "ldap";
 		}
 	    if(!is_object($user)){
 	        $user = parent::identify($account, $password);
+	        echo "dao"
 	    }
 	}else{
 		$user = parent::identify($account, $password);
 	}
+	return false;
     return $user;
 }

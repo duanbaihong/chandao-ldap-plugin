94faf4ebecb039cba1e95e8e7461bdb0a9e8bdd9
duanbaihhu <duanbaihu>
2020-05-28 09:34:10 +0800

    dd

--- module/user/js/login.js
+++ module/user/js/login.js
@@ -1,3 +1,6 @@
+// Prevent login page show in a iframe modal
+if(window.self !== window.top) window.top.location.href = window.location.href;
+
 $(document).ready(function()
 {
     /* Fix bug for misc-ping */
